1eeea8510dea4cea751c63c6b5c1391c35c9a372
lucasmends <lucasmends93@gmail.com>
2014-10-12 15:57:29 -0300

    Atualização do programa 4 - adição da funcionalidade kickar cliente e listar os clientes connectados

---
+++ src/GUI/casa/Casa.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI.casa;
+
+import core.AcaoCasa;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author lucas
+ */
+public class Casa extends JPanel{
+    
+    private AcaoCasa acao;
+    
+    public Casa(){
+        super();
+    }
+}
